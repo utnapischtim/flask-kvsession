fcbe787233ab580f85ceff3c4efca1a49f30efa3
Pablo Panero <pablo.panero@cern.ch>
2020-05-06 11:17:29 +0200

    version: add version file

---
+++ flask_kvsession/version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Flask-KVSession
+# Copyright (C) 2013, 2014, 2015, 2016 CERN.
+#
+# Flask-Menu is free software; you can redistribute it and/or modify
+# it under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Version information for Flask-KVSession."""
+
+__version__ = "0.6.3"
